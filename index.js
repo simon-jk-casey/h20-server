a30fa5dee2224c26563b96f9055b34675077fe31
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-17 21:12:46 +1200

    init index

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+// const corsOptions = {} later
+
+//uncomment when setup
+// const api = require('./routes/api')
+// const passport = require('./passportSetup')
+
+// app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser()) // learn more how this works with express-session
+// app.use(express.static(path.join(__dirname, 'public'))) -- needed?
+app.use(require('express-session')({secret: 'squirrel', resave: false, saveUnitialized: false})) // read more on this
+
+//  uncomment and tidy when passport set
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+//  uncomment when api routes done
+// app.use('/api/v1', api)
+
+module.exports = app
