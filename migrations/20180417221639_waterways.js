3f9225b898de557c0417ae2cec096f8fa5b3cd5d
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-17 22:41:00 +1200

    rem col waterways mig

--- migrations/20180417221639_waterways.js
+++ migrations/20180417221639_waterways.js
@@ -3,7 +3,6 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('waterways', (table) => {
     table.increments('id').primary()
     table.string('waterwayName').notNullable()
-    table.integer('farmerId').unsigned().notNullable()
   })
 }
 
