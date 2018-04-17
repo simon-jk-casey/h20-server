2a06fd60fd22b4e96cafce77a5004e72e34ff463
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-17 22:38:37 +1200

    waterways migration

--- migrations/20180417221639_waterways.js
+++ migrations/20180417221639_waterways.js
@@ -1,8 +1,12 @@
 
-exports.up = function(knex, Promise) {
-  
-};
+exports.up = function (knex, Promise) {
+  return knex.schema.createTableIfNotExists('waterways', (table) => {
+    table.increments('id').primary()
+    table.string('waterwayName').notNullable()
+    table.integer('farmerId').unsigned().notNullable()
+  })
+}
 
-exports.down = function(knex, Promise) {
-  
-};
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('waterways')
+}
