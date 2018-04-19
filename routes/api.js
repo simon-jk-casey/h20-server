eae692f1de3c2e7e3cebb7a9d31947cbfb6be918
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-19 22:47:21 +1200

    beginngin routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
+// GETS
+
+// this is a bullshit get to test the api endpoint, hint: it works
+router.get('/', (req, res) => {
   console.log('hit')
+})
+
+// POSTS
+
+router.post('/enrol', (req, res) => {
+  console.log(req.body)
 })
 
 module.exports = router
