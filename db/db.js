a23b66797f8b294d9412dc29af4e85382cbf9451
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-19 22:04:12 +1200

    add data collection functions

--- db/db.js
+++ db/db.js
@@ -4,6 +4,19 @@ const knex = Knex(knexConfig)
 
 // ADDING DATA
 export function addUser (user) {
-  return knex(user)
+  return knex('users').insert(user)
 }
+
+export function addWaterway (waterway) {
+  return knex('waterways').insert(waterway)
+}
+
+export function addCollectionPoint (collPt) {
+  return knex('collectionPoints').insert(collPt)
+}
+
+export function addAssessment (assData) {
+  return knex('assessments').insert(assData)
+}
+
 // FETCHING DATA
