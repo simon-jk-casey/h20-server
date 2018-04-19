7881667d5247666a97693ba9ed3d504839d36219
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-19 21:57:50 +1200

    rem farmer table, add users

--- db/db.js
+++ db/db.js
@@ -1,3 +1,9 @@
 const Knex = require('knex')
 const knexConfig = require('./knexfile')[process.env.NODE_ENV || 'development']
 const knex = Knex(knexConfig)
+
+// ADDING DATA
+export function addUser (user) {
+  return knex(user)
+}
+// FETCHING DATA
--- migrations/20180419215019_users.js
+++ migrations/20180419215019_users.js
@@ -1,13 +1,14 @@
 
 exports.up = function (knex, Promise) {
-  return knex.schema.hasTable('farmers').then((exists) => {
+  return knex.schema.hasTable('users').then((exists) => {
     if (!exists) {
-      return knex.schema.createTable('farmers', (t) => {
+      return knex.schema.createTable('users', (t) => {
         t.increments('id').unsigned().primary()
         t.string('name').notNullable()
         t.string('password').notNullable()
         t.string('address').notNullable()
         t.string('phoneNum').notNullable().unique()
+        t.boolean('admin').notNullable()
       })
     }
   })
