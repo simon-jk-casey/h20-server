1a2f4ac756c2684fd1fa705561bdbe67c2af42a2
simon-jk-casey <jesusiwasevil@gmail.com>
2018-04-25 20:40:33 +1200

    added basic post routes

--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,37 @@ router.get('/', (req, res) => {
 // POSTS
 
 router.post('/enrol', (req, res) => {
-  console.log(req.body)
   db.addUser(req.body)
+  .then(() => {
+    res.sendStatus(201)
+  })
+  .catch((err) => {
+    throw err
+  })
+})
+
+router.post('/water', (req, res) => {
+  db.addWaterway(req.body)
+  .then(() => {
+    res.sendStatus(201)
+  })
+  .catch((err) => {
+    throw err
+  })
+})
+
+router.post('/collection', (req, res) => {
+  db.addCollectionPoint(req.body)
+  .then(() => {
+    res.sendStatus(201)
+  })
+  .catch((err) => {
+    throw err
+  })
+})
+
+router.post('/assessment', (req, res) => {
+  db.addAssessment(req.body)
   .then(() => {
     res.sendStatus(201)
   })
